719d6ed12f5d019de57ea3ee3bdcee22066acc82
andyDevMdg <andyavotiana@gmail.com>
2024-03-13 14:37:44 +0300

    style: transactions list wireframe

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,5 +1,10 @@
 export const TransactionsList = () => {
   return (
-    <div>TransactionsList</div>
-  )
+    <>
+      <div className="mt-10">
+        <div className="text-xl open-sans-basic">Transactions </div>
+        <div className="mt-1 border-2 border-zinc-500 rounded-md w-full h-52"></div>
+      </div>
+    </>
+  );
 }
