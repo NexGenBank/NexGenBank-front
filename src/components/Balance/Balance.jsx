039ce3b563e635d32b108e882882bb83a3ce6502
andyDevMdg <andyavotiana@gmail.com>
2024-03-15 20:32:46 +0300

    refactor: card balance design

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,46 +1,52 @@
+import { useEffect, useState } from "react";
+
 export const Balance = () => {
+  const [balance, setBalance] = useState("10 000 000");
+  const [expense, setExpense] = useState("20 000 000");
+  const [saving, setSaving] = useState("30 000 000");
+
   return (
     <>
       <div className="text-2xl open-sans-basic font-semibold">Bank</div>
       <div className="flex flex-nowrap gap-2 justify-evenly w-full ">
-        <div className="bg-white rounded-lg effect p-10 mt-4 h-48 flex flex-nowrap gap-10 min-w-[360px]">
+        <div className="bg-white rounded-lg card-effect p-10 mt-4 h-48 flex flex-nowrap gap-10 min-w-[360px]">
           <div className="text-xl open-sans-basic">
             Balance
             <div className="rounded-lg min-w-[150px] max-w-[250px] font-normal text-4xl">
-              $10 000 000
+              ${balance}
             </div>
           </div>
           <div className="flex flex-col flex-nowrap gap-5">
-            <div className="bg-zinc-900 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
+            <div className="bg-red-600 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
               Withdraw
             </div>
-            <div className="bg-zinc-900 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
+            <div className="bg-green-600 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
               Credit
             </div>
           </div>
         </div>
-        <div className="bg-white rounded-lg effect p-10 mt-4 h-48 flex flex-nowrap justify-evenly gap-10 min-w-[360px]">
+        <div className="bg-white rounded-lg card-effect p-10 mt-4 h-48 flex flex-nowrap justify-evenly gap-10 min-w-[360px]">
           <div className="text-xl open-sans-basic">
             EXPENSES
             <div className="rounded-lg min-w-[150px] max-w-[250px] font-normal text-4xl">
-              $20 000 000
+              ${expense}
             </div>
           </div>
           <div className="flex flex-col flex-nowrap gap-5">
-            <div className="bg-zinc-900 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
+            <div className="bg-blue-900 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
               Add
             </div>
           </div>
         </div>
-        <div className="bg-white rounded-lg effect p-10 mt-4 h-48 flex gap-10 min-w-[360px]">
+        <div className="bg-white rounded-lg card-effect p-10 mt-4 h-48 flex gap-10 min-w-[360px]">
           <div className="text-xl open-sans-basic">
             SAVINGS
             <div className="rounded-lg min-w-[150px] max-w-[250px] font-normal text-4xl">
-              $30 000 000
+              ${saving}
             </div>
           </div>
           <div className="flex flex-col flex-nowrap gap-5">
-            <div className="bg-zinc-900 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
+            <div className="bg-blue-900 w-28 h-fit text-zinc-50 px-6 py-3 flex justify-center rounded-xl">
               Add
             </div>
           </div>
